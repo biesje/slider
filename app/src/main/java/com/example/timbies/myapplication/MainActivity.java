03ae94e9dfa9a92513da437cd8efa78f6ed0a775
TimBies <TimBies>
2016-06-16 11:37:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/timbies/myapplication/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.timbies.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import com.example.timbies.myapplication.FirstFragment;
+import com.example.timbies.myapplication.R;
+
+public class MainActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager) findViewById(R.id.viewPager);
+        pager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            switch(pos) {
+
+                case 0: return FirstFragment.newInstance("FirstFragment, Instance 1");
+                case 1: return SecondFragment.newInstance("SecondFragment, Instance 1");
+                case 2: return ThirdFragment.newInstance("ThirdFragment, Paard");
+                case 3: return ThirdFragment.newInstance("ThirdFragment, Ezel");
+                case 4: return ThirdFragment.newInstance("ThirdFragment, Instance 3");
+                default: return ThirdFragment.newInstance("ThirdFragment, Default");
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 5;
+        }
+    }
+}
